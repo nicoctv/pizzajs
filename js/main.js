b58e1091930bef06989be4654802328c254d30fc
nicoctv <nicolas.ctv@gmail.com>
2024-04-21 19:44:54 -0300

    ultima entrega js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,162 @@
-let montoPrestamo = parseInt (prompt ("Ingresar el monto del prestamo"));
-let tasaInteres = parseFloat (prompt ("Ingresar la tasa de interes (%)"));
-let numeroCuotas = parseInt (prompt("Ingresar el numero de cuotas"));
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const tasaInteresDecimal = tasaInteres / 100;
-let factor = 1;
-for ( let i = 0; i < numeroCuotas; i++){
-    factor *= (1 + tasaInteresDecimal);
+const productos = [
+    {
+        id: "combo-01",
+        titulo: "COMBO 5 Mozzarella",
+        precio: 14000,
+        img: "",
+    },
+    {
+        id: "combo-02",
+        titulo: "COMBO 5 Especiales",
+        precio: 18500,
+        img: "./img/Combo2.webp",
+    },
+    {
+        id: "combo-03",
+        titulo: "COMBO 5 y 5",
+        precio: 25000,
+        img: "./img/Combo3.webp",
+    }
+];
+
+const contenedorProductos = document.querySelector("#productos");
+const carritoVacio = document.querySelector("#carrito-vacio");
+const carritoProductos = document.querySelector("#carrito-productos");
+const carritoTotal = document.querySelector("#carrito-total");
+const numerito = document.querySelector("#numerito");
+
+
+productos.forEach((producto) => {
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+        <img class="producto-img" src="${producto.img}" alt="${producto.titulo}">
+        <h3>${producto.titulo}</h3>
+        <p>$${producto.precio}</p>
+    `;
+
+    const btn = document.createElement("button");
+    btn.classList.add("producto-btn");
+    btn.innerText = "Agregar al carrito";
+
+    btn.addEventListener("click", () => {
+        agregarAlCarrito(producto);
+    })
+    div.append(btn);
+
+    let div2 = document.createElement("div");
+    div2.innerText = "3 y 6 cuotas sin interes!!";
+    div.append(div2);
+
+    contenedorProductos.append(div);
+})
+
+let botonesAgregar = document.querySelectorAll(".boton-agregar");
+botonesAgregar.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+        let id = e.target.id;
+        let productoAsignado = productos.find(prod => prod.id === id);
+        agregarAlCarrito(productoAsignado);
+    })
+})
+
+function actualizarCarrito() {
+    if (carrito.length === 0) {
+        carritoVacio.classList.remove("d-none");
+        carritoProductos.classList.add("d-none");
+    } else {
+        carritoVacio.classList.add("d-none");
+        carritoProductos.classList.remove("d-none");
+
+        carritoProductos.innerHTML = "";
+        carrito.forEach(producto => {
+            const div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+                <h3>${producto.titulo}</h3>
+                <p>$${producto.precio}</p>
+                <p>Cant: ${producto.cantidad}</p>
+                <p>Subt: $${producto.cantidad * producto.precio}</p>
+            `;
+
+            const btnRestar = document.createElement("button");
+            btnRestar.classList.add("carrito-producto-btn");
+            btnRestar.innerText = "⬇️";
+            btnRestar.addEventListener("click", () => {
+                restarDelCarrito(producto);
+            })
+            div.append(btnRestar);
+
+            const btnSumar = document.createElement("button");
+            btnSumar.classList.add("carrito-producto-btn");
+            btnSumar.innerText = "⬆️";
+            btnSumar.addEventListener("click", () => {
+                sumarDelCarrito(producto);
+            })
+            div.append(btnSumar);
+
+            const btnEliminar = document.createElement("button");
+            btnEliminar.classList.add("carrito-producto-btn");
+            btnEliminar.innerText = "✖️";
+            btnEliminar.addEventListener("click", () => {
+                borrarDelCarrito(producto);
+            })
+            div.append(btnEliminar);
+
+            carritoProductos.append(div);
+        })
+    }
+    calcularNumerito();
+    actualizarTotal();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-const cuotaMensual = (montoPrestamo * tasaInteresDecimal * factor / (factor - 1));
-const montoTotal = cuotaMensual * numeroCuotas;
+const agregarAlCarrito = (producto) => {
+    const itemEncontrado = carrito.find(item => item.titulo === producto.titulo);
+    if (itemEncontrado) {
+        itemEncontrado.cantidad++;
+    } else {
+        carrito.push({...producto, cantidad: 1});
+    }
 
-console.log("Cuota mensual",cuotaMensual)
-console.log("Monto total a pagar",montoTotal)
+    actualizarCarrito();
+
+    Toastify({
+        text: "Producto agregado al carrito.",
+        gravity: "bottom",
+        position: "right",
+        duration: 1000
+    }).showToast();
+}
+
+const borrarDelCarrito = (producto) => {
+    const prodIndex = carrito.findIndex(item => item.titulo === producto.titulo);
+    carrito.splice(prodIndex, 1);
+    actualizarCarrito();
+}
+
+const restarDelCarrito = (producto) => {
+    if (producto.cantidad !== 1) {
+        producto.cantidad--;
+    }
+    actualizarCarrito();
+}
+
+const sumarDelCarrito = (producto) => {
+    producto.cantidad++;
+    actualizarCarrito();
+}
+
+const actualizarTotal = () => {
+    const total = carrito.reduce((acc, prod) => acc + (prod.precio * prod.cantidad), 0);
+    carritoTotal.innerText = `$${total}`;
+}
+
+const calcularNumerito = () => {
+    const numeritoTotal = carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
+    numerito.innerText = numeritoTotal;
+}
+
+actualizarCarrito();
